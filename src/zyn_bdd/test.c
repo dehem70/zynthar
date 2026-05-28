09fb02e61389ff33bfb82578071726d73f947529
dehem70 <denis.mugnier@laposte.net>
2026-05-28 19:23:28 +0200

    ajout outil new_c dans ./temp

---
+++ src/zyn_bdd/test.c
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                      //
+//                                          ZYNTHAR v0.1                                                //
+//                                                                                                      //
+// Auteur : Dehem70                                                                                     //
+// Date   : 28/05/2026                                                                                  //
+//                                                                                                      //
+// test  ;                                                                                              //
+// utilisation :                                                                                        //
+//                                                                                                      //
+//                                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sqlite3.h>
+
+// En-têtes requis sous Linux/POSIX pour manipuler 'struct stat' et 'mkdir'
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <../../include/zynthar.h>
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+
+        return 1;
+    }
+
+
+    return 0;
+}
