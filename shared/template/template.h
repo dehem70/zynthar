e7b1e7d95619b616f962e964839020ef975d0ff1
dehem70 <denis.mugnier@laposte.net>
2026-05-30 14:29:03 +0200

    ajout template.h + corrections

---
+++ shared/template/template.h
@@ -0,0 +1,12 @@
+#ifndef ----------_H
+#define ----------_H
+
+/* =============================================================================
+ * Projet Zynthar v.xx
+ * Fichier : ----------.h
+ * Date    : xx/xx/xxxx
+ * ============================================================================= */
+
+// Placez ici vos macros, structures locales et prototypes de fonctions
+
+#endif // ----------_H
